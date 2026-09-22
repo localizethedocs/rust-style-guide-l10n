201c683da7c35dcc1fd4b970320bf0a88706589c
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-22 14:02:24 +0000

    Deploying to pages from @ localizethedocs/rust-style-guide-l10n@6e2c3c0cf964f0ee05457c9265f2e3d77621cd91 🚀

---
+++ ltd-config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["nightly", "Nightly"],
+    ["beta",    "Beta"],
+    ["stable",  "Stable"],
+    ["1",       "Release 1.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/rust-style-guide-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/rust-style-guide-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/rust-style-guide-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/rust-style-guide-l10n"],
+  ]
+};
